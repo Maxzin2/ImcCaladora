b95b1f92b0b2568b7a4a973bc4df3bb37938dcec
Maxzin2 <zinetico22@icloud.com>
2024-05-12 13:36:21 +0200

    mostrar messagem de preencha os campos.

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,24 +3,40 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.SnackbarContentLayout
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val edtpeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtaltura = findViewById<TextInputEditText>(R.id.edt_altura)
         val btncalcular = findViewById<Button>(R.id.btn_calcular)
 
         btncalcular.setOnClickListener {
-            val peso: Float = edtpeso.text.toString().toFloat()
-            val altura : Float = edtaltura.text.toString().toFloat()
+            val pesoString: String = edtpeso.text.toString()
+            val alturaString : String = edtaltura.text.toString()
 
-            val alturaQ2 = altura * altura
-            val resultado = peso/alturaQ2
-            println("Max ação do botão" + resultado)
+            if (pesoString == "" || alturaString == "") {
+                // Mostrar messagem para usuario
+                Snackbar
+                    .make(
+                        edtpeso,
+                        "Preencha todos os campos vazios",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+
+            }else {
+                val peso = pesoString.toFloat()
+                val altura = alturaString.toFloat()
+
+                val alturaQ2 = altura * altura
+                val resultado = peso/alturaQ2
+                println("Max acao do botao" + resultado)
+            }
 
         }
     }
